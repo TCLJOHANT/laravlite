c57b6365866e537489d4ed5c9f730d24332e8490
Johan Ch <developer88tcljohant@gmail.com>
2025-11-18 20:23:08 -0500

    feat: defaults midlewires advanced

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,24 @@ use LaravLite\Config;
 use LaravLite\Middleware\AuthMiddleware;
 use LaravLite\Http\Response;
 use LaravLite\Facades\Route;
+use LaravLite\Middleware\CsrfMiddleware;
+use LaravLite\Middleware\GlobalMiddleware;
 
 require __DIR__ . '/helpers.php';
-require __DIR__ . '/../routes/web.php';
+Route::aliasMiddleware('auth', AuthMiddleware::class);
+Route::aliasMiddleware('global', GlobalMiddleware::class);
+Route::aliasMiddleware('csrf', CsrfMiddleware::class);
+//Route::aliasMiddleware('api', ApiMiddleware::class);
+
+Route::group(['middleware' => 'csrf'], function () {
+    require __DIR__ . '/../routes/web.php';
+});
 
 Config::load(
     __DIR__ . '/../config',
     __DIR__ . '/cache/config.php'
 );
 
-Route::aliasMiddleware('auth', AuthMiddleware::class);
 Route::group(['prefix' => 'api'], function () {
     require __DIR__ . '/../routes/api.php';
 });
